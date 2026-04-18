aa31012d5651fbd3a72c557795300d4fc4d431b2
radiantNearl26 <asmitdaschoudhury@proton.me>
2026-04-18 18:55:38 +0530

    FIX: Updated alert-battery.sh to show alert only when it's discharging.

--- .scripts/alert-battery.sh
+++ .scripts/alert-battery.sh
@@ -11,19 +11,19 @@ BATTERY_STATUS=$(cat /sys/class/power_supply/BAT0/status)
 BATTERY_CAPACITY=$(cat /sys/class/power_supply/BAT0/capacity)
 
 # Check if battery percentage is below the low threshold
-if [ "$BATTERY_CAPACITY" -le "$LOW_THRESHOLD" ] && [ "$BATTERY_CAPACITY" -gt "$CRITICAL_THRESHOLD" ]; then
+if [ "$BATTERY_CAPACITY" -le "$LOW_THRESHOLD" ] && [ "$BATTERY_CAPACITY" -gt "$CRITICAL_THRESHOLD" ] && [ "$BATTERY_STATUS" = "Discharging" ]; then
 # Display a low battery notification
     notify-send "Warning!!" "Battery Low! Battery is at ${BATTERY_CAPACITY}%" -i "battery-caution"
 fi
 
 # Check if battery percentage is below the critical threshold
-if [ "$BATTERY_CAPACITY" -le "$CRITICAL_THRESHOLD" ] && [ "$BATTERY_CAPACITY" -gt "$EMERGENCY_THRESHOLD" ]; then
+if [ "$BATTERY_CAPACITY" -le "$CRITICAL_THRESHOLD" ] && [ "$BATTERY_CAPACITY" -gt "$EMERGENCY_THRESHOLD" ] && [ "$BATTERY_STATUS" = "Discharging" ]; then
 # Display a critical battery notification
     notify-send "CrItIcAl!!" "Almost Empty! Battery is at ${BATTERY_CAPACITY}%!" -i "battery-empty"
 fi
 
 # Check if battery percentage is below the emergency threshold
-if [ "$BATTERY_CAPACITY" -le "$EMERGENCY_THRESHOLD" ]; then
+if [ "$BATTERY_CAPACITY" -le "$EMERGENCY_THRESHOLD" ] && [ "$BATTERY_STATUS" = "Discharging" ]; then
 # Display an emergency battery notification
     notify-send "EMERGENCY!!" "Battery is at ${BATTERY_CAPACITY}%! Plug in immediately!" -i "battery-missing"
 fi
