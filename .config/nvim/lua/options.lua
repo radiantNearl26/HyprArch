6e67bd7f1bd363eee3461d4f7f358fde2d2b4db9
radiantNearl26 <asmitdaschoudhury@proton.me>
2026-06-17 14:10:01 +0530

    UPD: Switch to relative line numbers in neovim & some misc changes.

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -16,8 +16,8 @@ vim.opt.shiftwidth = 4   -- insert 4 spaces on a tab
 vim.opt.expandtab = true -- tabs are spaces, mainly because of python
 
 -- UI config
-vim.opt.number = true          -- show absolute number
-vim.opt.relativenumber = false -- add numbers to each line on the left side
+vim.opt.number = false          -- show absolute number
+vim.opt.relativenumber = true -- add numbers to each line on the left side
 vim.opt.cursorline = true      -- highlight cursor line underneath the cursor horizontally
 vim.opt.splitbelow = true      -- open new vertical split bottom
 vim.opt.splitright = true      -- open new horizontal splits right
